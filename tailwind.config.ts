ef4673e92b6c2c1d68b5a488bd1b9491dc3e93f9
xianort <xianort99@gmail.com>
2024-12-23 14:54:37 +0300

    Add project colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,15 @@ import type { Config } from "tailwindcss";
 
 export default {
     content: ["./src/**/*.{html,htm}"],
-    theme: {},
+    theme: {
+        colors: {
+            grey: {
+                700: "hsl(0, 0%, 20%)",
+                800: "hsl(0, 0%, 12%)",
+                900: "hsl(0, 0%, 8%)",
+            },
+            white: "hsl(0, 0%, 100%)",
+            green: "hsl(75, 94%, 57%)",
+        },
+    },
 } satisfies Config;
-
